585b6dc85e057e29ab19e8f573af377928ece86e
Romeo Folie <romeofolie1@gmail.com>
2020-12-23 00:15:25 +0000

    worked on accordion, carousel, and footer

--- js/index.js
+++ js/index.js
@@ -1,7 +1,6 @@
 var acc = document.getElementsByClassName("accordion");
-var i;
 
-for (i = 0; i < acc.length; i++) {
+for (let i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function() {
     /* Toggle between adding and removing the "active" class,
     to highlight the button that controls the panel */
@@ -9,10 +8,23 @@ for (i = 0; i < acc.length; i++) {
 
     /* Toggle between hiding and showing the active panel */
     var panel = this.nextElementSibling;
-    if (panel.style.display === "block") {
-      panel.style.display = "none";
+
+    if(panel.style.maxHeight){
+      panel.style.maxHeight = null;
+      panel.style.borderBottom = "none";
     } else {
-      panel.style.display = "block";
+      let scrollHeight = panel.scrollHeight + 50;
+      panel.style.maxHeight = scrollHeight + 'px';
+      panel.style.borderBottom = "0.5px solid #909ABB";
     }
   });
 }
+
+
+// let resources = document.getElementsByClassName("resource-item");
+
+// for(let i = 0; i < resources.length; i++){
+//   resources[i].addEventListener("onmouseover", function() {
+    
+//   })
+// } 
